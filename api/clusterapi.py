a57d46f4fc44341e69dd4c53c2f7e2d6e0bbdaa1
jeanarnaud ritouret <jeanarnaudritouret@gmail.com>
2021-08-26 14:50:14 +0200

    fifith push

--- api/clusterapi.py
+++ api/clusterapi.py
@@ -5,15 +5,13 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-app = FastAPI()
-
-app.add_middleware(
+""" app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Allows all origins
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
-)
+) """
 
 @app.get('/')
 def index():
